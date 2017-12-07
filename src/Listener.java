3453587699122f1d3373c772845a2bbba63ce8c2
Marcel <marcel.konstantinov@gmail.com>
2017-12-07 17:58:30 +0100

    Tastaturbefehle umgesetzt

--- src/Listener.java
+++ src/Listener.java
@@ -1,6 +1,11 @@
 import org.zeromq.*;
 import com.google.gson.*;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+
 
 public class Listener {
 
@@ -24,18 +29,38 @@ public class Listener {
         subscriber.recv(0);
         
         String msg = "";
-        while (!msg.equalsIgnoreCase("END")) {
+        try {
+			Robot robot = new Robot();
+	        while (!msg.equalsIgnoreCase("END")) {
 
-            msg = new String(subscriber.recv(0));
-        	Gson gson = new Gson();
-        	Event event = gson.fromJson(msg, Event.class);
-        	
-        	if(event.getType()=="Gesture"){
-        		
-        	}
-        	
-            System.out.println(event.getParameters().getName());
+	            msg = new String(subscriber.recv(0));
+	        	Gson gson = new Gson();
+	        	Event event = gson.fromJson(msg, Event.class);
+	        	
+	        	if(event.getType().equals("Gesture")){
+        			System.out.println("Gesture");
+	        		if(event.getParameters().getName().equals("Swipe R")){
+	        			System.out.println("Right Swipe");
+	        			robot.keyPress(KeyEvent.VK_RIGHT);
+	        			robot.keyRelease(KeyEvent.VK_RIGHT);
+	        		}else if(event.getParameters().getName().equals("Swipe L")){
+	        			System.out.println("Left Swipe");
+	        			robot.keyPress(KeyEvent.VK_LEFT);
+	        			robot.keyRelease(KeyEvent.VK_LEFT);
+	        		}else if(event.getParameters().getName().equals("Check Mark")){
+	        			System.out.println("Check Mark");
+	        			robot.keyPress(KeyEvent.VK_ENTER);
+	        			robot.keyRelease(KeyEvent.VK_ENTER);
+	        		}
+	        	}
+	        	
+	            System.out.println(msg);
 
-	}
+		}
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
